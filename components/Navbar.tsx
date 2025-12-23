0ec5022812ac590549b2d956806ded8119509590
Joseph France <jafcxm@umsystem.edu>
2025-12-22 22:23:17 -0600

    Adjusted some margins for mobiledevices.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <nav className='fixed top-0 w-full bg-[#0a0a0a] border-b-2 border-gray-500 z-50'>
             <div className="max-w-6xl mx-auto px-6 py-4">
-                    <div className="flex justify-center gap-16">
+                    <div className="flex justify-center gap-13">
                         <a href="#about" className="text-1xl text-gray-500 hover:scale-110 transition-transform duration-300 hover:text-gray-300 transition">
                             About
                         </a>
