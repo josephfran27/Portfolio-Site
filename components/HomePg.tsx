0cbb77b33aea1064ba878f13c0d006bf7cc87647
Joseph France <jafcxm@umsystem.edu>
2025-12-27 12:09:46 -0600

    Added expected graduation date.

--- components/HomePg.tsx
+++ components/HomePg.tsx
@@ -28,7 +28,7 @@ export default function HomePg() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                         </svg>
                         <p className="text-base sm:text-xl text-gray-400">
-                            Bachelor of Science in Computer Science with a Minor in Mathematics
+                            Bachelor of Science in Computer Science with a Minor in Mathematics (Anticipated: May 2027)
                         </p>
                     </div>
                 </div>
